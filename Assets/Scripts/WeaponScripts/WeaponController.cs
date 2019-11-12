2f02586a6d1f011f183d8b3aba6e662ced577381
michael whiddett <michael-whiddett@hotmail.com>
2019-11-12 18:43:04 +0000

    Added WeaponController Script Added WeaponConfig Script

---
+++ Assets/Scripts/WeaponScripts/WeaponController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum NameWeapon
+{
+    MELEE,
+    PISTOL,
+    MP5,
+    M3,
+    AK,
+    AWP,
+    FIRE,
+    ROCKET
+
+}
+public class WeaponController : MonoBehaviour
+{
+
+    public DefaultConfig defaultConfig;
+    public NameWeapon nameWp;
+
+
+    protected PlayerAnimations playerAnim;
+    protected float lastShot;
+
+    public int gunIndex;
+    public int currentBullet;
+    public int bulletMaximum;
+
+
+
+    void Awake()
+    {
+        playerAnim = GetComponentInParent<PlayerAnimations>();
+        currentBullet = bulletMaximum;
+
+    }
+
+    public void CallAttack()
+    {
+        if (Time.time > lastShot + defaultConfig.fireRate)
+        {
+            if (currentBullet > 0)
+            {
+                ProcessAttack();
+                playerAnim.AttackAnimation();
+                lastShot = Time.time;
+                currentBullet--;
+            }
+            else
+            {
+
+            }
+        }
+    }
+
+    public virtual void ProcessAttack() {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
