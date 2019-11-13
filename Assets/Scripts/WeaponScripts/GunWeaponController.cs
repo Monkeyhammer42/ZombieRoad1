373f73a8d8d6477abed32e99c16d8625504f19a0
monkeyhammer42 <michael-whiddett@hotmail.com>
2019-11-13 15:51:25 +0000

    Added PlayerArmScript Added GunWeaponControllerScript Added WeaponManagerScripts Added MeleeWeaponControllerScript

---
+++ Assets/Scripts/WeaponScripts/GunWeaponController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunWeaponController : WeaponController
+{
+    public Transform spawnPoint;
+    public GameObject bulletPrefab;
+    public ParticleSystem fx_Shot;
+    public GameObject fx_BulletFall;
+    private Collider2D fireCollider;
+    private WaitForSeconds wait_Time = new WaitForSeconds(0.02f);
+    private WaitForSeconds fire_ColliderWait = new WaitForSeconds(0.02f);
+
+
+
+    void Start()
+    {
+        
+    }
+
+    public override void ProcessAttack()
+    {
+        //base.ProcessAttack();
+        switch (nameWp)
+        {
+            case NameWeapon.PISTOL:
+                break;
+            case NameWeapon.MP5:
+                break;
+            case NameWeapon.M3:
+                break;
+            case NameWeapon.AWP:
+                break;
+            case NameWeapon.AK:
+                break;
+            case NameWeapon.FIRE:
+                break;
+            case NameWeapon.ROCKET:
+                break;
+           
+
+        }
+        
+    }
+
+    IEnumerator WaitForShootEffect()
+    {
+        yield return wait_Time;
+        fx_Shot.Play();
+
+
+    }
+
+    IEnumerator ActiveFireCollider()
+    {
+        fireCollider.enabled = true;
+        yield return fire_ColliderWait;
+        fireCollider.enabled = false;
+    }
+
+
+
+
+
+
+
+
+}
+
---
+++ Assets/Scripts/WeaponScripts/WeaponManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    public List<WeaponController> weapons_Unlocked;
+
+    public WeaponController[] total_Weapons;
+    [HideInInspector]
+    public WeaponController current_Weapon;
+    private int current_Weapon_Index;
+
+
+    private TypeControlAttack current_Type_Control;
+    private PlayerArmController[] armController;
+    private PlayerAnimations playerAnim;
+    private bool isShooting;
+
+    private void Awake()
+    {
+        playerAnim = GetComponent<PlayerAnimations>();
+        LoadActiveWeapons();
+        current_Weapon_Index = 1;
+
+    }
+    void Start()
+    {
+        armController = GetComponentsInChildren<PlayerArmController>();
+        playerAnim.SwitchWeaponAnimation((int)weapons_Unlocked[current_Weapon_Index].defaultConfig.typeWeapon);
+
+    }
+
+    void LoadActiveWeapons()
+    {
+        weapons_Unlocked.Add(total_Weapons[0]);
+        for(int i = 1; i < total_Weapons.Length; i++)
+        {
+            weapons_Unlocked.Add(total_Weapons[i]);
+        }
+    }
+
+    public void SwitchWeapon()
+    {
+        current_Weapon_Index++;
+        current_Weapon_Index = (current_Weapon_Index >= weapons_Unlocked.Count) ? 0 : current_Weapon_Index;
+
+        playerAnim.SwitchWeaponAnimation((int)weapons_Unlocked[current_Weapon_Index].defaultConfig.typeWeapon);
+    }
+
+
+
+
+
+}
